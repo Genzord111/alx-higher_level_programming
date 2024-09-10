342fd5d645a64a45e09fdc489a3578a7c921cd7e
Genzord111 <feliciensolomon5@gmail.com>
2024-09-10 15:26:28 -0700

    first edit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        a = i
+        if (ord(a) < 128 and ord(a) > 96):
+            a = chr(ord(a)-32)
+        print("{}".format(a), end = '')
+
+    print('')
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    for i in str(number):
+        i = i
+    print(i, end ='')
+    
+    return int(i)
