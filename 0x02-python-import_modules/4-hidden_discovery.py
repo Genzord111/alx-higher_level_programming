617ccfa7145be11cd4c2bf6dac590d742c28c109
Genzord111 <feliciensolomon5@gmail.com>
2024-09-16 02:23:45 -0700

    updated 4*.py code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-import sys, hidden
 
-dir(hidden.pyc)
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    for i in names:
+        if i.startswith("__", 0, 2):
+            continue
+        else:
+            print(i)
