298536ab4dda7561ebae52da0aa2411a49390f31
Genzord111 <feliciensolomon5@gmail.com>
2024-09-18 17:10:00 -0700

    code edit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,24 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    else:
-        new_list = []
-        for key, value in a_dictionary.items():
-            if value is not None:
-                new_list.append((key, value))
-
-        if len(new_list) == 0:
-            return None
-        else:
-            max_num = new_list[0][1]
-            name = ""
-
-            for i in new_list:
-                if i[1] > max_num:
-                    max_num = i[1]
-                    name = i[0]
-
-    return name
+    return max(a_dictionary, key=a_dictionary.get)
