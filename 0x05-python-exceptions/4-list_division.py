09db4477deea834f717f03ce753bebe1864d446d
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-01 00:05:11 +0000

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,9 +18,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             new_list.append(0)
-
-        i += 1
-        j += 1
-        count += 1
-
+        finally:
+            i += 1
+            j += 1
+            count += 1
     return new_list
