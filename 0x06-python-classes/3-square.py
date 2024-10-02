ddfe5ca8a8475109f228e7c92188aabb9ea4e085
Genzord111 <feliciensolomon5@gmail.com>
2024-10-02 05:45:25 -0700

    om

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is the 1-square module"""
+
+
+class Square:
+    """Creates an instance of the class Square"""
+
+    def __init__(self, size=0):
+        """Initializes size with a positive integer value"""
+        try:
+            if (size < 0):
+                raise ValueError('size must be >= 0')
+            self._Square__size = int(size)
+        except TypeError:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """returns the area of the square"""
+        return ((self._Square__size)**2)
