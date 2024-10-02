cb39ec9cf8987d28d784d5e1188c9be0a5090db0
Genzord111 <feliciensolomon5@gmail.com>
2024-10-02 04:54:41 -0700

    m

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+
+"""This is the 1-square module"""
+
+
 class Square:
+    """Creates an instance of the class Square"""
 
     def __init__(self, _Square__size):
+        """Initializes private instannce attribute with a value"""
+
         self._Square__size = _Square__size
