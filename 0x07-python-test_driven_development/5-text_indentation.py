e958a2b4031f7c8fbc4f44a1f84d114e43082c74
Genzord111 <feliciensolomon5@gmail.com>
2024-10-11 06:36:33 -0700

    m

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+"""This module contains a function for string formating"""
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines after each of character: ., ? and :"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be string")
+    new_str = ""
+    size = len(text)
+    i = 0
+
+    while(i < size):
+        new_str += text[i]
+        check = text[i]
+        if (check == '.' or check == ':' or check == '?'):
+            new_str += '\n'
+            j = i + 1
+
+            while text[j] == " ":
+                j += 1
+            i = j
+
+        else:
+            i += 1
+
+    print(new_str)
