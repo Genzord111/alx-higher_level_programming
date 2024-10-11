7efff80a36df8538c3952b328b234ebf642a0417
Genzord111 <feliciensolomon5@gmail.com>
2024-10-11 08:06:08 -0700

    m

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+"""This is a unittest module """
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Class initiates testing of max_integer function"""
+
+    def test_max_integer(self):
+        """Test 1"""
+        self.assertEqual(max_integer([1, 7, 4, 1.5]), 7)
+        self.assertEqual(max_integer([10.5, 6.78, 5]), 10.5)
