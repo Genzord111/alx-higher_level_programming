399be8013c964855a475aece3ee4b2fd75f3f5c4
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-15 17:08:08 +0000

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
 """This modeule contains the t0_json_string function"""
 
 
+import json
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)"""
-
     return json.dumps(my_obj)
