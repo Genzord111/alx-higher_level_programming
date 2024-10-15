2e95953cd426512038320af041ab0d7fb9b710f5
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-15 23:56:41 +0000

    Update 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module contains pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """Prints a pascal triangle"""
+
+    one = [1, 1]
+    my_list = [[1], [1, 1]]
+    if n <= 0:
+        return []
+    elif n == 1:
+        return my_list[0]
+    elif n == 2:
+        return my_list
+
+    if n > 2:
+        for i in range(n - 2):
+            one = [1, 1]
+            mid = []
+            new_list = my_list[-1]
+            i = 0
+            while i != len(new_list) - 1:
+                mid.append(new_list[i] + new_list[i+1])
+                i += 1
+            one[1:1] = mid
+            my_list.append(one)
