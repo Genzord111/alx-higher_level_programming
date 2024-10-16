0731a25efa07afcadd9974c2eef9e57911cba3fb
Genzord111 <feliciensolomon5@gmail.com>
2024-10-15 17:59:39 -0700

    m

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module contains class_to_json function"""
+
+
+class Student:
+    """This creates a class of student details"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new = {}
+            for ele in attrs:
+                for keys, value in self.__dict__.items():
+                    if keys == ele:
+                        new[keys] = value
+            return new
+
+    def reload_from_json(self, json):
+        """assigns new attributes from json"""
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
