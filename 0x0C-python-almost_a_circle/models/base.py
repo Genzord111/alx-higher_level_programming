b4772f6f047d7374fc6dc4ec73cac995dfdebaa8
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-20 21:18:16 +0000

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module contains the Base class"""
+import json
 
 
 class Base:
@@ -14,3 +15,20 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """returns dictionary of instance attributes as JSON string"""
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file of Base"""
+        atrribute_lists = []
+        if list_objs is not None:
+           for i in list_objs:
+                if isinstance(i, cls):
+                    file_name = f"{i.__class__.__name__}.json"
+                    atrribute_lists.append(i.to_dictionary())
+        
+        with open(file_name, 'w') as f:
+            json.dump(atrribute_lists, f)
