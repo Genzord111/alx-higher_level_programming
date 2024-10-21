d412b752a52c736d78c6623a55472704913f0bf3
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-21 13:55:26 +0000

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,16 +9,24 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Instantiation with id or num of onject"""
+        """Instantiation with id value or number of object"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
             self.id = id
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns dictionary of instance attributes as JSON string"""
         return json.dumps(list_dictionaries)
+    
+    @staticmethod
+    def from_json_string(json_string):
+        attribute_list = []
+        if json_string is not None:
+            attribute_list = json.loads(json_string)
+        return attribute_list
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -32,3 +40,22 @@ class Base:
         
         with open(file_name, 'w') as f:
             json.dump(atrribute_lists, f)
+
+    @classmethod
+    def create(cls, **dictionary):
+        container_class = cls(1, 1, 1)
+        container_class.update(**dictionary)
+        return container_class
+    
+    @classmethod
+    def load_from_file(cls):
+        class_list = []
+        file_name = f"{cls.__name__}.json"
+        print(file_name)
+        try:
+            with open(file_name, 'r') as f:
+                class_list = json.load(file_name)
+        except IOError:
+            pass
+
+        return class_list
