8b19fc76124c88042331fa39884e20661da8a4c9
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-22 01:39:55 +0000

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,14 +5,75 @@ import unittest
 
 class TestRectangle(unittest.TestCase):
     """Test class"""
+
+    def setUp(self):
+        """Sets up instances for testing"""
+        self.r1 = Rectangle(5, 3)
+        self.r2 = Rectangle(10, 2)
+        self.r3 = Rectangle(10, 2, 0, 0, 12)
+
+    def tearDown(self) -> None:
+        """Teardown"""
+        return super().tearDown()
+
     def test_init_(self):
-        """Tests class instantiation"""
-        r1 = Rectangle(5, 3)
-        r2 = Rectangle(10, 2)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(print(r1.id), f"1")
-        self.assertEqual(print(r2.id), f"2")
-        self.assertEqual(print(r3.id), f"1")
+        """Tests initialization of class"""
+
+        self.assertEqual(self.r1.id, 1)
+        self.assertEqual(self.r2.id, 2)
+        self.assertEqual(self.r3.id, 12)
+
+        self.assertIsInstance(self.r1, Rectangle)
+        self.assertIsInstance(self.r2, Base)
+
+        self.assertFalse(self.r1 == self.r2)
+        self.assertIsNot(self.r1, self.r2)
+
+        with self.assertRaises(TypeError) as context:
+            Rectangle()
+        self.assertEqual(str(context.exception), "Rectangle.__init__() \
+missing 2 required positional arguments: 'width' and 'height'")
+
+    def test_invalid_dimension_type(self):
+        """Test width type validation"""
+        with self.assertRaises(TypeError) as context:
+            Rectangle("invalid", 2)
+        self.assertEqual(str(context.exception), "width must be an integer")
+
+        with self.assertRaises(TypeError) as context:
+            Rectangle(7, {})
+        self.assertEqual(str(context.exception), "height must be an integer")
+
+        with self.assertRaises(ValueError) as context:
+            Rectangle(-1, 5)
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+        with self.assertRaises(ValueError) as context:
+            Rectangle(1, -5)
+        self.assertEqual(str(context.exception), "height must be > 0")
+
+        with self.assertRaises(TypeError) as context:
+            Rectangle(1, 5, 0, [])
+        self.assertEqual(str(context.exception), "y must be an integer")
+
+        with self.assertRaises(ValueError) as context:
+            Rectangle(1, 5, -2, 7)
+        self.assertEqual(str(context.exception), "x must be >= 0")
+
+    def test_width_setter(self):
+        """Test width setter validation"""
+        with self.assertRaises(ValueError) as context:
+            self.r1.width = 0
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+        with self.assertRaises(ValueError) as context:
+            self.r1.width = 0
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+    def test_area(self):
+        """Test area function"""
+        self.assertEqual(self.r1.area(), 15)
+        self.assertEqual(self.r3.area(), 20)
 
 
 if __name__=='__main__':
