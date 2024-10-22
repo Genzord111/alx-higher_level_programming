e133cd5b3752ccbc126d96080f7aad5a01b3c7ef
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2024-10-22 00:13:39 +0000

    Update test_rectangle.py

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,18 @@
+"""This module contain unittest for the rectangle class"""
+from models.rectangle import Rectangle
+from models.square import Square
+import unittest
+
+class TestRectangle(unittest.TestCase):
+    """Test class"""
+    def test_init_(self):
+        r1 = Rectangle()
+        r2 = Rectangle(10, 2)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(print(r1.id), f"1")
+        self.assertEqual(print(r2.id), f"2")
+        self.assertEqual(print(r3.id), f"1")
+
+
+if __name__=='__main__':
+    unittest.main()
