ba71ad80402a5f085e2efc21f3403a0fc7e563dd
Genzord111 <feliciensolomon5@gmail.com>
2024-10-21 18:57:02 -0700

    m

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,20 +1,19 @@
 """This module contain unittest for the rectangle class"""
+from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 import unittest
+
 
 class TestRectangle(unittest.TestCase):
     """Test class"""
 
     def setUp(self):
         """Sets up instances for testing"""
+        Base._Base__nb_objects = 0
         self.r1 = Rectangle(5, 3)
         self.r2 = Rectangle(10, 2)
         self.r3 = Rectangle(10, 2, 0, 0, 12)
-
-    def tearDown(self) -> None:
-        """Teardown"""
-        return super().tearDown()
 
     def test_init_(self):
         """Tests initialization of class"""
@@ -31,7 +30,7 @@ class TestRectangle(unittest.TestCase):
 
         with self.assertRaises(TypeError) as context:
             Rectangle()
-        self.assertEqual(str(context.exception), "Rectangle.__init__() \
+        self.assertEqual(str(context.exception), "__init__() \
 missing 2 required positional arguments: 'width' and 'height'")
 
     def test_invalid_dimension_type(self):
@@ -76,5 +75,5 @@ missing 2 required positional arguments: 'width' and 'height'")
         self.assertEqual(self.r3.area(), 20)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
