3f6135f55b68dd02d57cae6db73339475bda6da1
Genzord111 <feliciensolomon5@gmail.com>
2024-10-21 18:35:45 -0700

    added test for Rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,7 +7,7 @@ class TestRectangle(unittest.TestCase):
     """Test class"""
     def test_init_(self):
         """Tests class instantiation"""
-        r1 = Rectangle()
+        r1 = Rectangle(5, 3)
         r2 = Rectangle(10, 2)
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(print(r1.id), f"1")
