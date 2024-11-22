b6b08236283ea79ae2b3d8690e8cfc46ab30b1b9
Genzord111 <feliciensolomon5@gmail.com>
2024-11-21 20:30:11 -0800

    m

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,8 @@
 -- script that lists all genres from hbtn_0d_tvshows
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
+SELECT tv_genres.name, COUNT(*) AS number_of_shows
+FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,7 @@
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
---Each record should display: tv_genres.name
+-- Each record should display tv_genres.name
 -- Results must be sorted in ascending order by the genre name
+SELECT tv_genres.name
+FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.show_id = 8;
